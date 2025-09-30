fef28b6f1be2b59a2fba2f041128ed76a227f871
annaprokofeva <anna.prokofeva14@gmail.com>
2025-09-30 12:00:08 +0300

    update

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -2,6 +2,8 @@ fullname = str(input(("Введите ФИО: ")))
 fullname = " ".join(fullname.split())
 fullname1 = fullname.split()
 
+fullname = fullname.replace(' ', "")
+
 if len(fullname1)==3:
     surname, name, secondname = fullname1
 initials=f"{surname[0]}{name[0]}{secondname[0]}."
