0e3803b4cd7aa46b8e6b0902d79e19e1d6548cc7
annaprokofeva <anna.prokofeva14@gmail.com>
2025-10-07 11:20:37 +0300

    fix_update

---
+++ src/lab02/arrays_03.py
@@ -0,0 +1,14 @@
+#3_flatten
+def flatten(mat: list[list | tuple]) -> list:
+    result = []
+    for row in mat:
+        if isinstance(row,(tuple,list)):
+            result.extend(row)
+        else:
+            raise TypeError
+    return(result)
+
+print(flatten([[1, 2], [3, 4]]))
+print(flatten(([1, 2], (3, 4, 5))))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -6,7 +6,7 @@ def transpose(mat: list[list[float | int]]):
     cols=len(mat[0])
     for row in mat:
         if len(row)!=cols:
-            return ("ValueError")
+            raise ValueError
     new_mat = [[mat[i][j] for i in range(rows)] for j in range(cols)]
     return new_mat
 print(transpose([[1, 2, 3]]))
@@ -14,35 +14,3 @@ print(transpose([[1],[2], [3]]))
 print(transpose([[1,2],[3,4]]))
 print(transpose([[1,2],[3]]))
 print(transpose([[]]))
-
-#2_row_sums
-def row_sums(mat: list[list[float | int]]):
-    if not mat:
-        return []
-    rows = len(mat)
-    cols = len(mat[0])
-    for row in mat:
-        if len(row) != cols:
-            return ("ValueError")
-    sums=[sum(row) for row in mat]
-    return sums
-print(row_sums([[1,2,3], [4,5,6]]))
-print(row_sums([[-1, 1], [10, -10]]))
-print(row_sums([[0,0], [0,0]]))
-print(row_sums([[1,2], [3]]))
-
-#3_col_sums
-def col_sums(mat: list[list[float | int]]):
-    if not mat:
-        return []
-    rows = len(mat)
-    cols = len(mat[0])
-    for row in mat:
-        if len(row) != cols:
-            return ("ValueError")
-    sums = [sum(mat[i][j] for i in range(rows)) for j in range(cols)]
-    return sums
-print(col_sums([[1, 2, 3], [4, 5, 6]]))
-print(col_sums([[-1, 1], [10, -10]]))
-print(col_sums([[0, 0], [0, 0]]))
-print(col_sums([[1, 2], [3]]))
