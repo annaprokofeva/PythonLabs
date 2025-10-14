dd686d16269a0bdb35fb5acb2364ef86f090b1d1
annaprokofeva <anna.prokofeva14@gmail.com>
2025-10-14 11:06:11 +0300

    lab03

---
+++ src/lab03/normalize_01.py
@@ -0,0 +1,20 @@
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    if casefold == True:
+        text = text.casefold()
+    if yo2e == True:
+        text = text.replace('ё', 'е').replace('Ё', 'Е')
+    text = text.replace('\t', ' ').replace('\r', ' ').replace('\n', ' ')
+    text = ' '.join(text.split())
+    text = text.strip()
+    if text is None:
+        raise ValueError
+    if not isinstance(text, str):
+        raise TypeError
+    if len(text) == 0:
+        return ""
+    return text
+
+print(normalize("ПрИвЕт\nМИр\t"))
+print(normalize("ёжик, Ёлка"))
+print(normalize("Hello\r\nWorld"))
+print(normalize(" двойные пробелы "))
